a1787636912e6343c7ea070c51d085725ac3eba1
Gonzalo Bide <gonzalo.bide@gmail.com>
2019-02-26 21:29:59 -0300

    Server clean

--- server.js
+++ server.js
@@ -3,10 +3,3 @@ const app = http.createServer((res,req)=>{})
 app.listen(8000);
 const appBO = http.createServer((res,req)=>{})
 app.listen(9000);
-
-http.get({
-	hostname: 'localhost',
-	port: "80",
-	path: "",
-	agent: false
-}, (r) => {});
