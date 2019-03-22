30a2517ef4ad3df964684c65039462bd4f425116
Gonzalo Bide <gonzalo.bide@gmail.com>
2019-03-22 02:03:56 -0300

    mejoras

--- chat/server/router.js
+++ chat/server/router.js
@@ -8,18 +8,18 @@ module.exports = async (req,res) => {
         'Access-Control-Allow-Origin': '*'
     }
     function appJs() {
-        let app = fs.createReadStream(__dirname+"/app.js");
+        let app = fs.createReadStream(__dirname+"/scripts/app.js");
         res.writeHead(Status.OK.code,headers)
         app.pipe(res);    
     }
     function indexHtml() {
-        let index = fs.createReadStream(__dirname+"/index.html");
+        let index = fs.createReadStream(__dirname+"/views/index.html");
         res.writeHead(Status.OK.code,headers)
         index.pipe(res);    
     }
     let routes = new Map();
     routes.set('/app.js', appJs)
-    routes.set('/index-ws.js', indexHtml)
+    routes.set('/index.html', indexHtml)
     routes.set('/', indexHtml)
     let route = routes.get(req.url)
     if (typeof route === 'function')
