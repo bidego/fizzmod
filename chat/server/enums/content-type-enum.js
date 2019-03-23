a352a02c9a5d6a2100d942707474f63a2424975f
Gonzalo Bide <gonzalo.bide@gmail.com>
2019-03-22 23:04:04 -0300

    mejoras

---
+++ chat/server/enums/content-type-enum.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ContentType: {
+        HTML: "text/html",
+        CSS: "text/css",
+        PLAIN: "text/plain",
+        JS: "application/javascript",
+        JSON: "application/json"
+    }
+}
--- chat/server/router.js
+++ chat/server/router.js
@@ -1,27 +1,32 @@
 const fs = require('fs')
 const { Status } = require('./enums/status-enum')
+const { ContentType } = require('./enums/content-type-enum')
 
 module.exports = async (req,res) => {
-    let headers = {
-        "content-type":"text/html",
-        "Access-Control-Allow-Headers":"*",
-        'Access-Control-Allow-Origin': '*'
+    
+    let headers = (type) => { return {
+            "content-type":type,
+            "Access-Control-Allow-Headers":"*",
+            'Access-Control-Allow-Origin': '*'
+        }
     }
-    function appJs() {
-        let app = fs.createReadStream(__dirname+"/scripts/app.js");
-        res.writeHead(Status.OK.code,headers)
-        app.pipe(res);    
-    }
-    function indexHtml() {
-        let index = fs.createReadStream(__dirname+"/views/index.html");
-        res.writeHead(Status.OK.code,headers)
-        index.pipe(res);    
+
+    function fetchFile(r) {
+        let file = fs.createReadStream(__dirname + "/" + r.path);
+        res.writeHead(Status.OK.code,headers(r.type))
+        file.pipe(res);    
     }
     let routes = new Map();
-    routes.set('/app.js', appJs)
-    routes.set('/index.html', indexHtml)
-    routes.set('/', indexHtml)
+    routes.set('/app.js', { path: "scripts/app.js", type: ContentType.JS })
+    routes.set('/styles.css', { path: "views/styles.css", type: ContentType.CSS })
+    routes.set('/index.html', { path: "views/index.html", type: ContentType.HTML })
+    routes.set('/', { path: "views/index.html", type: ContentType.HTML })
+    
     let route = routes.get(req.url)
-    if (typeof route === 'function')
-        route()
+    if (typeof route === 'object')
+        fetchFile(route)
+    else {
+        res.writeHead(Status.NO_PAGE.code, Status.NO_PAGE.message)
+        res.end("no file")
+    }
 }
